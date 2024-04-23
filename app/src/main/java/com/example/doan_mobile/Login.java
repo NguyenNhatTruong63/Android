19329d1e8b0d2274d063dc2333f1af355850bf48
NguyenNhatTruong63 <21130228@st.hcmuaf.edu.vn>
2024-04-23 20:51:59 +0700

    food_drinks

---
+++ app/src/main/java/com/example/doan_mobile/Login.java
@@ -0,0 +1,34 @@
+package com.example.doan_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        textView = findViewById(R.id.title);
+        button = findViewById(R.id.signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/doan_mobile/Trangchu.java
@@ -0,0 +1,44 @@
+package com.example.doan_mobile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trangchu extends AppCompatActivity {
+    private ViewPager viewPager;
+    private PhotoAdapter photoAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_trangchu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        viewPager = findViewById(R.id.viewpager);
+
+        photoAdapter = new PhotoAdapter(this, getListPhoto());
+        viewPager.setAdapter(photoAdapter);
+    }
+
+    private List<Photo> getListPhoto() {
+        List<Photo> list = new ArrayList<>();
+        list.add(new Photo(R.drawable.logo1));
+        list.add(new Photo(R.drawable.logo2));
+        list.add(new Photo(R.drawable.logo3));
+
+        return list;
+
+    }
+
+}
