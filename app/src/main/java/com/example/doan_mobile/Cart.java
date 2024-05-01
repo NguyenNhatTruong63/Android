3197294382259117e0506824b533b18a92bacc78
Toan546579 <letruongtoan29082008@gmail.com>
2024-05-01 10:39:14 +0700

    drink

--- app/src/main/java/com/example/doan_mobile/Cart.java
+++ app/src/main/java/com/example/doan_mobile/Cart.java
@@ -1,5 +1,4 @@
 package com.example.doan_mobile;
-
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
@@ -10,21 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ThongTin_Drink extends AppCompatActivity {
+public class Cart extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_thongtin_drink);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_cart);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cart), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        TextView txt = findViewById(R.id.textView_information);
+        TextView txt = findViewById(R.id.scroll_cart);
         txt.setMovementMethod(new ScrollingMovementMethod());
     }
 }
